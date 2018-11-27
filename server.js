d48e3a4ad43bf27d52d70d8ce570967cca739191
Nagatani <nagatani@work-ark.jp>
2018-11-27 15:46:06 +0900

    💤

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+});
+
 io.on('connection', function( socket ){
   console.log('a user connected');
   io.emit('chat message', 'new user connected!!');
